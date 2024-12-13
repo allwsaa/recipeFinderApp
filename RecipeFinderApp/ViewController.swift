84e50362066ea7eb2b846562804c069d742cd063
allwsaa <bekbosynova.alima@inbox.ru>
2024-12-13 22:56:35 +0500

    Initial Commit

---
+++ RecipeFinderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeFinderApp
+//
+//  Created by ntvlbl on 13.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
